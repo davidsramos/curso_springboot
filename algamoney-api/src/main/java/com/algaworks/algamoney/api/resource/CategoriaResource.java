f9ba87f487d0598b14f06d108aab4398166b95cc
David Ramos <davidsramos@gmail.com>
2021-02-07 17:11:37 +0100

    tratamento de retorno

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.algaworks.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,11 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar() {
-		return this.categoriaRepository.findAll();
+	public ResponseEntity<?> listar() {
+		
+		List<Categoria> categorias = this.categoriaRepository.findAll();
+		
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
 	}
 }
  
